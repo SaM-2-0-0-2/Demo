7b6eb56eb115b1b22150db3e3b1d975a878dd9ff
dipshi verma <vermadipshi70@gmail.com>
2025-07-23 22:42:45 +0530

    commit-3

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo{
 	public static void main(String...args){
 		System.out.println("HELLO WORLD");
-		System.out.println("Hello Dipshi Verma");
+		System.out.println("Hello Dipshi");
 		System.out.println("Hello Shriram");
 	}
 }
